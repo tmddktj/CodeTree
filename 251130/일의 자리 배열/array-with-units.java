e3568105ad1dfa26857f67a9deaae58d7181b9d1
tmddktj <tmddktj@gmail.com>
2025-11-30 20:08:09 +0900

    Title: 일의 자리 배열, Time: 131ms, Memory: 10MB, Status: Passed - Codetree

--- 251130/일의 자리 배열/array-with-units.java
+++ 251130/일의 자리 배열/array-with-units.java
@@ -8,11 +8,12 @@ public class Main {
         numbers[0] = sc.nextInt();
         numbers[1] = sc.nextInt();
 
-        System.out.print(numbers[0] + " " + numbers[1]);
-
         for(int i = 2; i<10; i++){
             numbers[i] = (numbers[i-2] + numbers[i-1]) % 10;
-            System.out.print(" "  +  numbers[i]);
+        }
+
+        for (int number : numbers){
+            System.out.print(number + " ");
         }
     }
 }
