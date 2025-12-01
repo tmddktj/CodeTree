a7b20ab6c033c231fdc282cdcaa4765b7cf1f88a
tmddktj <tmddktj@gmail.com>
2025-12-01 23:00:47 +0900

    Title: 1부터의 합, Time: 133ms, Memory: 10MB, Status: Passed - Codetree

---
+++ 251201/1부터의 합/sum-from-the-one.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        int sum = 0;
+        int num = 0;
+
+        for(int i = 1; i<101; i++){
+            sum += i;
+            if(sum>=n){
+                num = i;
+                break;
+            }
+        }
+        System.out.println(num);
+    }
+}
