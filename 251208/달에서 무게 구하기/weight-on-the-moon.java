ddd754742eee39757e6fc776ec404ee76cd7855c
tmddktj <tmddktj@gmail.com>
2025-12-08 21:19:49 +0900

    Title: 달에서 무게 구하기, Time: 95ms, Memory: 9MB, Status: Passed - Codetree

---
+++ 251208/달에서 무게 구하기/weight-on-the-moon.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        int a = 13;
+        double g = 0.165;
+
+        System.out.printf(a+" * %.6f = %.6f", g, a*g);
+        
+    }
+}
