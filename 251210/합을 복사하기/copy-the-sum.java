e96f331dce4f76d3d1eb15452db249f6836671cf
tmddktj <tmddktj@gmail.com>
2025-12-10 18:59:23 +0900

    Title: 합을 복사하기, Time: 95ms, Memory: 9MB, Status: Passed - Codetree

---
+++ 251210/합을 복사하기/copy-the-sum.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+        int a = 1;
+        int b = 2;
+        int c = 3;
+        int sum = a+b+c;
+        a = sum;
+        b = sum;
+        c = sum;
+        System.out.println(a+" "+b+" "+c);
+    }
+}
