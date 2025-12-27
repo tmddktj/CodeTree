913b6fc1e86f0188808e5c7fcc367d6f7ab0f1de
tmddktj <tmddktj@gmail.com>
2025-12-27 22:36:25 +0900

    Title: 세 정수의 최솟값, Time: 124ms, Memory: 10MB, Status: Passed - Codetree

--- 251227/세 정수의 최솟값/minimum-of-three-numbers.java
+++ 251227/세 정수의 최솟값/minimum-of-three-numbers.java
@@ -3,15 +3,16 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int[] arr = new int[3];
-        int min = arr[0];
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int c = sc.nextInt();
 
-        for(int i = 0; i<3; i++){
-            arr[i] = sc.nextInt();
-            if(arr[i] < min){
-                min = arr[i];
-            }
+        if(a<=b && a<=c){
+            System.out.print(a);
+        } else if(b<=a && b<=c){
+            System.out.print(b);
+        } else{
+            System.out.print(c);
         }
-        System.out.print(min);
     }
 }
