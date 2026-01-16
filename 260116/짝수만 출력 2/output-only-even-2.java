b7d72cd22c8888595f7bada7b7e7c997ae918f7e
tmddktj <tmddktj@gmail.com>
2026-01-16 20:00:07 +0900

    Title: 짝수만 출력 2, Time: 133ms, Memory: 10MB, Status: Passed - Codetree

---
+++ 260116/짝수만 출력 2/output-only-even-2.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int b = sc.nextInt();
+        int a = sc.nextInt();
+        while(b >= a){
+            System.out.printf("%d\t", b);
+            b -= 2;
+        }
+    }
+}
