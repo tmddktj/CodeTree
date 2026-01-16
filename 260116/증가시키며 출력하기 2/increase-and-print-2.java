67f916fe8d85dc33c74073fdf56397db55aa44b8
tmddktj <tmddktj@gmail.com>
2026-01-16 19:30:44 +0900

    Title: 증가시키며 출력하기 2, Time: 87ms, Memory: 9MB, Status: Passed - Codetree

---
+++ 260116/증가시키며 출력하기 2/increase-and-print-2.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        int n = 10;
+
+        while(n <= 26){
+            System.out.printf("%d\t", n);
+            n++;
+        }
+    }
+}
