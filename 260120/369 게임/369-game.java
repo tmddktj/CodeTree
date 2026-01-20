bef83672aacbf17cd87fae74e3867b925d9430ad
tmddktj <tmddktj@gmail.com>
2026-01-20 16:05:09 +0900

    Title: 369 게임, Time: 125ms, Memory: 10MB, Status: Wrong Answer - Codetree

--- 260120/369 게임/369-game.java
+++ 260120/369 게임/369-game.java
@@ -5,10 +5,28 @@ public class Main {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
         for(int i = 1; i <= n; i++){
-            if(i % 3 == 0 || i % 10 == 3 || i % 10 == 6 || i % 10 == 9){
-                System.out.print(0 + " ");
+            boolean check = false;
+
+            if(i % 3 == 0){
+                check = true;
+            }
+            int num = i;
+            while(num > 0){
+                int lastNum = i % 10;
+                if(lastNum == 3 || lastNum == 6 || lastNum == 9){
+                    check = true;
+                    break;
+                }
+                num /= 10;
+            }
+
+            if(check){
+                System.out.print(0);
             } else{
-                System.out.print(i + " ");
+                System.out.print(i);
+            }
+            if(i < n){
+                System.out.print(" ");
             }
         }
     }
